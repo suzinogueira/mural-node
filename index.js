05d735d7fc7f1e65539e27e521a7cbcb6d8b6464
Suzi Nogueira <asuzinogueira@gmail.com>
2021-09-14 09:35:37 -0300

    organizando a separaçao de rotas

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const PORT= 3000;
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const posts = require('./model/posts');
+const apiRoute = require('./routes/api');
+
+const path = require("path");
 
 const app = express();
 
 
+app.use('/api', apiRoute );
+
+app.use('/',express.static(path.join(__dirname,"public")));
 
 
-app.get("/all", (req, res)=>{
-    res.json(JSON.stringify(posts.getAll()));
-})
-
-app.post("/new", bodyParser.json(), (req, res)=>{
-
-    let title  = req.body.title;
-    let description = req.body.description;
-
-    posts.newPost(title, description);
-    
-    res.send("Post adicionado");
-})
 
 app.listen(PORT, ()=>{
     console.log("Server running on port", PORT)
