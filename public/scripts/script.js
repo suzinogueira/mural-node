99777471c81dd5309d0910522bcb8cee585aec9c
Suzi Nogueira <asuzinogueira@gmail.com>
2021-09-18 21:27:46 -0300

    finalizando o front e usando cors

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 function updatePosts(){
 
-   fetch("http://localhost:3000/api/all").then( res=>{
+   fetch("http://192.168.0.197:3000/api/all").then( res=>{
        return res.json();
     }).then(json =>{
 
@@ -31,4 +31,27 @@ function updatePosts(){
 
 function newPost(){
 
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("desc").value;
+
+    console.log(title,description);
+
+    let post = {title, description};
+
+    const options = {
+        method:"POST",
+        headers: new Headers({
+            'content-type': 'application/json'}),
+        body: JSON.stringify(post) 
+    }
+
+    fetch("http://localhost:3000/api/new", options).then(res=>{
+        console.log(res);
+        updatePosts();
+
+        document.getElementById("title").value="";
+        document.getElementById("desc").value="";
+    })
+
+    
 }
